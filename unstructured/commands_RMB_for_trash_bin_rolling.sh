6f2a41a2e8f224ea0710bfa77c8704b478345895
kotaro-kajitsuka-a <tactile.kajitsuka@aist.go.jp>
2026-07-08 15:53:03 +0900

    Add configuration for RealXarm7DualDemoEnvTrashBinRolling and update rollout script

--- unstructured/commands_RMB_for_trash_bin_rolling.sh
+++ unstructured/commands_RMB_for_trash_bin_rolling.sh
@@ -1,19 +1,22 @@
 
+uv run python -m robo_manip_baselines.policy.rl_policy_dual.rl_tasks.view_single_aruco_marker
+
+
 
 
 
 #########################################################################
 
 python ./robo_manip_baselines/bin/CreatePpoCusMetaInfo.py \
-  --config ./robo_manip_baselines/rl_tasks/robo_manip_baselines/rl_tasks/dual_joint_pos_vel_marker_fixed_gripper.json \
+  --config ./robo_manip_baselines/rl_tasks/dual_joint_pos_vel_marker_fixed_gripper.json \
   --output "robo_manip_baselines/checkpoint/PpoCus/TrashBinRolling/model_meta_info.pkl" \
   --force
 
-python ./robo_manip_baselines/bin/Rollout.py RLPolicy RealXarm7Demo \
+python ./robo_manip_baselines/bin/Rollout.py RLPolicyDual RealXarm7DualDemo \
   --checkpoint robo_manip_baselines/checkpoint/PpoCus/TrashBinRolling/ckpt_41.pt \
   --wait_before_start \
   --skip_draw 50000 \
-  --config ./robo_manip_baselines/envs/configs/RealXarm7DualDemo.yaml \
+  --config ./robo_manip_baselines/envs/configs/RealXarm7DualDemoEnvTrashBinRolling.yaml \
   --world_idx_repeat_count 45
 
 ###########################################################################
