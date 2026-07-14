30c494ded466fa4ee80c1f049f1d4d2d1983813e
kotaro-kajitsuka-a <tactile.kajitsuka@aist.go.jp>
2026-07-14 17:33:44 +0900

    Update trash_bin_rolling configuration and adjust rollout script for improved task execution

--- unstructured/commands_RMB_for_trash_bin_rolling.sh
+++ unstructured/commands_RMB_for_trash_bin_rolling.sh
@@ -7,15 +7,16 @@ python robo_manip_baselines/policy/rl_policy_dual/rl_tasks/view_single_aruco_mar
 #########################################################################
 
 python ./robo_manip_baselines/bin/CreatePpoCusMetaInfo.py \
-  --config ./robo_manip_baselines/rl_tasks/dual_joint_pos_vel_marker_fixed_gripper.json \
+  --config ./robo_manip_baselines/rl_tasks/trash_bin_rolling.json \
   --output "robo_manip_baselines/checkpoint/PpoCus/TrashBinRolling/model_meta_info.pkl" \
   --force
 
-python ./robo_manip_baselines/bin/Rollout.py RLPolicyDual RealXarm7DualDemo \
-  --checkpoint robo_manip_baselines/checkpoint/PpoCus/TrashBinRolling/ckpt_41.pt \
+python ./robo_manip_baselines/bin/Rollout.py Sac RealXarm7DualFixedGripperDemo \
+  --checkpoint robo_manip_baselines/checkpoint/PpoCus/TrashBinRolling/0714_ckpt_471.pt \
   --wait_before_start \
   --skip_draw 50000 \
-  --config ./robo_manip_baselines/envs/configs/RealXarm7DualDemoEnvTrashBinRolling.yaml \
+  --save_rollout \
+  --config ./robo_manip_baselines/envs/configs/RealXarm7DualDemoEnv.yaml \
   --world_idx_repeat_count 45
 
 ###########################################################################
