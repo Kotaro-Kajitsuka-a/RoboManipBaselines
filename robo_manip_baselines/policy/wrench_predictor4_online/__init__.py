12aa71f880b3e1df234b23cacbc1ff697b2da994
kotaro-kajitsuka-a <tactile.kajitsuka@aist.go.jp>
2026-08-05 12:53:26 +0900

    Remove WrenchPredictor4Online classes and related scripts to streamline the codebase

--- robo_manip_baselines/policy/wrench_predictor4_online/__init__.py
+++ robo_manip_baselines/policy/wrench_predictor4_online/__init__.py
@@ -1,7 +1,5 @@
-from .TrainWrenchPredictor4Online import TrainWrenchPredictor4Online
 from .WrenchPredictor4OnlineDataset import WrenchPredictor4OnlineDataset
 
 __all__ = [
-    "TrainWrenchPredictor4Online",
     "WrenchPredictor4OnlineDataset",
 ]
