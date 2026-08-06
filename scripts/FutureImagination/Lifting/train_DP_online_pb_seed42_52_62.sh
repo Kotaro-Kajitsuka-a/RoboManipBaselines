4e8fd486b2f5dcab2468bb526f6d3e9914008cb6
kotaro-kajitsuka-a <tactile.kajitsuka@aist.go.jp>
2026-08-06 15:42:51 +0900

    Add scripts for training with constant-PB dataset and update training configurations for seeds 42, 52, and 62

--- scripts/FutureImagination/Lifting/train_DP_online_pb_seed42_52_62.sh
+++ scripts/FutureImagination/Lifting/train_DP_online_pb_seed42_52_62.sh
@@ -1,6 +1,13 @@
 #!/usr/bin/env bash
 set -e
 
+# Add the causal online-PB trajectories used to train Diffusion Policy.
+python robo_manip_baselines/policy/wrench_predictor4_online/AddOnlinePbToDataset.py \
+  robo_manip_baselines/dataset/LiftingAB_B_only \
+  0 \
+  --checkpoint robo_manip_baselines/checkpoint/WrenchPredictor4/LiftingAB_B_only/policy_best.ckpt \
+  --lr 6e-3 \
+  --overwrite
 
 # Train seed 42.
 python robo_manip_baselines/bin/Train.py DiffusionPolicy \
@@ -14,8 +21,8 @@ python robo_manip_baselines/bin/Train.py DiffusionPolicy \
 
 # Train seed 52.
 python robo_manip_baselines/bin/Train.py DiffusionPolicy \
-  --dataset_dir robo_manip_baselines/dataset/LiftingAB_B_only_ConstantPB \
-  --checkpoint_dir robo_manip_baselines/checkpoint/DiffusionPolicy/LiftingAB_B_only_ConstantPB_seed52 \
+  --dataset_dir robo_manip_baselines/dataset/LiftingAB_B_only \
+  --checkpoint_dir robo_manip_baselines/checkpoint/DiffusionPolicy/LiftingAB_B_only_Online_seed52 \
   --camera_names \
   --state_keys measured_eef_pose measured_gripper_joint_pos measured_tblock_pose material_property \
   --action_keys command_eef_pose command_gripper_joint_pos \
@@ -25,8 +32,8 @@ python robo_manip_baselines/bin/Train.py DiffusionPolicy \
 
 # Train seed 62.
 python robo_manip_baselines/bin/Train.py DiffusionPolicy \
-  --dataset_dir robo_manip_baselines/dataset/LiftingAB_B_only_ConstantPB \
-  --checkpoint_dir robo_manip_baselines/checkpoint/DiffusionPolicy/LiftingAB_B_only_ConstantPB_seed62 \
+  --dataset_dir robo_manip_baselines/dataset/LiftingAB_B_only \
+  --checkpoint_dir robo_manip_baselines/checkpoint/DiffusionPolicy/LiftingAB_B_only_Online_seed62 \
   --camera_names \
   --state_keys measured_eef_pose measured_gripper_joint_pos measured_tblock_pose material_property \
   --action_keys command_eef_pose command_gripper_joint_pos \
