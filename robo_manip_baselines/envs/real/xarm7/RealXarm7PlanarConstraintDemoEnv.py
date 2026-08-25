85c2c06129a65581d80cb6e4683b1949a6a200e1
kotaro-kajitsuka-a <tactile.kajitsuka@aist.go.jp>
2026-08-25 20:35:26 +0900

    Increase max end-effector speed and z-drop tolerance; implement robot reset logic in RealXarm7PlanarConstraintDemoEnv

--- robo_manip_baselines/envs/real/xarm7/RealXarm7PlanarConstraintDemoEnv.py
+++ robo_manip_baselines/envs/real/xarm7/RealXarm7PlanarConstraintDemoEnv.py
@@ -7,7 +7,7 @@ from .RealXarm7FixedGripperDemoEnv import RealXarm7FixedGripperDemoEnv
 
 
 class PlanarConstraintArmManager(ArmManager):
-    max_eef_speed = 0.05  # [m/s]
+    max_eef_speed = 0.10  # [m/s]
 
     def set_command_joint_pos(self, arm_joint_pos, gripper_joint_pos):
         raise RuntimeError(
@@ -35,7 +35,8 @@ class PlanarConstraintArmManager(ArmManager):
 
 
 class RealXarm7PlanarConstraintDemoEnv(RealXarm7FixedGripperDemoEnv):
-    max_eef_z_drop = 0.003  # [m]
+    max_eef_z_drop = 0.005  # [m]
+    reset_joint_pos_tolerance = np.deg2rad(0.1)  # [rad]
 
     def __init__(
         self,
@@ -48,6 +49,31 @@ class RealXarm7PlanarConstraintDemoEnv(RealXarm7FixedGripperDemoEnv):
         self.eef_pin_model = pin.buildModelFromUrdf(arm_config.arm_urdf_path)
         self.eef_pin_data = self.eef_pin_model.createData()
         self.original_eef_z = self._get_eef_z(arm_config.init_arm_joint_pos)
+
+    def _reset_robot(self):
+        print(
+            f"[{self.__class__.__name__}] Start moving the robot to the reset position."
+        )
+
+        arm_config = self.body_config_list[0]
+        obs = self._get_obs()
+        while True:
+            arm_joint_pos = obs["joint_pos"][arm_config.arm_joint_idxes]
+            arm_joint_pos_error = arm_config.init_arm_joint_pos - arm_joint_pos
+            if np.max(np.abs(arm_joint_pos_error)) <= self.reset_joint_pos_tolerance:
+                break
+
+            self._set_action(
+                self.init_qpos,
+                duration=self.dt,
+                joint_vel_limit_scale=0.1,
+                wait=True,
+            )
+            obs = self._get_obs()
+
+        print(
+            f"[{self.__class__.__name__}] Finish moving the robot to the reset position."
+        )
 
     def _get_obs(self):
         obs = super()._get_obs()
