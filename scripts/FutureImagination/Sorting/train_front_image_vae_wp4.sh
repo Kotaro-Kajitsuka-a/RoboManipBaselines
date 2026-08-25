b957b8041913b0d51b0f5551cc4ab91529a2477a
Kotaro-Kajitsuka-a <tactile.kajitsuka@aist.go.jp>
2026-08-25 15:06:39 +0900

    Refactor RealXarm7DemoEnv.yaml to enable front camera and add train_front_image_vae_wp4.sh script for VAE training and evaluation

---
+++ scripts/FutureImagination/Sorting/train_front_image_vae_wp4.sh
@@ -0,0 +1,45 @@
+#!/usr/bin/env bash
+set -e
+
+# Run from the repository root with the virtual environment activated.
+train_dataset_dir=robo_manip_baselines/dataset/0825_DatasetSorting_20260825_134504/training
+validation_dataset_dir=robo_manip_baselines/dataset/0825_DatasetSorting_20260825_134504/validation
+vae_output_dir=robo_manip_baselines/checkpoint/ImageVAE/0825_DatasetSorting_20260825_front_9
+wp4_checkpoint_dir=robo_manip_baselines/checkpoint/WrenchPredictor4/0825_DatasetSorting_20260825_front_image_vae_9_joint_pos
+
+python robo_manip_baselines/misc/futureimagination/TrainImageVAE.py \
+  --train_dataset_dir "$train_dataset_dir" \
+  --validation_dataset_dir "$validation_dataset_dir" \
+  --output_dir "$vae_output_dir" \
+  --camera_name front \
+  --latent_dim 64
+
+python robo_manip_baselines/misc/futureimagination/AddImageVAEFeature.py \
+  "$train_dataset_dir" \
+  --checkpoint "$vae_output_dir/final_model" \
+  --camera_name front \
+  --overwrite
+
+python robo_manip_baselines/misc/futureimagination/AddImageVAEFeature.py \
+  "$validation_dataset_dir" \
+  --checkpoint "$vae_output_dir/final_model" \
+  --camera_name front \
+  --overwrite
+
+python robo_manip_baselines/bin/Train.py WrenchPredictor4 \
+  --dataset_dir "$train_dataset_dir" \
+  --val_dataset_dir "$validation_dataset_dir" \
+  --checkpoint_dir "$wp4_checkpoint_dir" \
+  --camera_names \
+  --state_keys measured_joint_pos \
+  --action_keys command_joint_pos \
+  --image_feature_key image_vae_front_9 \
+  --wrench_source_key measured_eef_wrench \
+  --pb_dim 1 \
+  --wrench_loss_weight 0.0 \
+  --num_epochs 500
+
+python robo_manip_baselines/policy/wrench_predictor4/EvalWrenchPredictor4ImageFeatureSweepDir.py \
+  "$wp4_checkpoint_dir/policy_best.ckpt" \
+  "$validation_dataset_dir" \
+  --material_object_ids 0 1 2
