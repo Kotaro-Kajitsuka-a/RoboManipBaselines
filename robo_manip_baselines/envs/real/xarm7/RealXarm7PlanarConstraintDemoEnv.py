29025da5250264293b55c021de0d67980b30835b
kotaro-kajitsuka-a <tactile.kajitsuka@aist.go.jp>
2026-08-25 19:37:04 +0900

    Refactor PlanarConstraintArmManager to improve eef pose command handling

--- robo_manip_baselines/envs/real/xarm7/RealXarm7PlanarConstraintDemoEnv.py
+++ robo_manip_baselines/envs/real/xarm7/RealXarm7PlanarConstraintDemoEnv.py
@@ -1,3 +1,4 @@
+import numpy as np
 import pinocchio as pin
 
 from robo_manip_baselines.common import ArmManager, get_se3_from_pose
@@ -6,6 +7,8 @@ from .RealXarm7FixedGripperDemoEnv import RealXarm7FixedGripperDemoEnv
 
 
 class PlanarConstraintArmManager(ArmManager):
+    max_eef_speed = 0.02  # [m/s]
+
     def set_command_joint_pos(self, arm_joint_pos, gripper_joint_pos):
         raise RuntimeError(
             f"[{self.__class__.__name__}] Joint position commands bypass the planar constraint. Use command_eef_pose or command_eef_pose_rel."
@@ -13,14 +16,22 @@ class PlanarConstraintArmManager(ArmManager):
 
     def set_command_eef_pose(self, eef_pose):
         if isinstance(eef_pose, pin.SE3):
-            target_se3 = eef_pose.copy()
+            next_target_se3 = eef_pose.copy()
         else:
-            target_se3 = get_se3_from_pose(eef_pose)
+            next_target_se3 = get_se3_from_pose(eef_pose)
 
-        target_se3.translation[2] = self._original_target_se3.translation[2]
-        target_se3.rotation = self._original_target_se3.rotation.copy()
+        next_target_se3.translation[2] = self._original_target_se3.translation[2]
+        next_target_se3.rotation = self._original_target_se3.rotation.copy()
 
-        super().set_command_eef_pose(target_se3)
+        translation_delta = next_target_se3.translation - self.target_se3.translation
+        translation_distance = np.linalg.norm(translation_delta)
+        max_translation_distance = self.max_eef_speed * self.env.unwrapped.dt
+        if translation_distance > max_translation_distance:
+            next_target_se3.translation[:] = self.target_se3.translation + (
+                max_translation_distance * translation_delta / translation_distance
+            )
+
+        super().set_command_eef_pose(next_target_se3)
 
 
 class RealXarm7PlanarConstraintDemoEnv(RealXarm7FixedGripperDemoEnv):
