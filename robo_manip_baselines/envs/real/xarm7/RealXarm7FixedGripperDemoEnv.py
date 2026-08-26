938d8fe8cac6a116234e26a6d862bc2e3704407a
Kotaro-Kajitsuka-a <tactile.kajitsuka@aist.go.jp>
2026-08-26 10:46:47 +0900

    Update gripper joint position to zero in grasp phase and fixed gripper demo environment; set collision sensitivity in planar constraint demo environment

--- robo_manip_baselines/envs/real/xarm7/RealXarm7FixedGripperDemoEnv.py
+++ robo_manip_baselines/envs/real/xarm7/RealXarm7FixedGripperDemoEnv.py
@@ -8,7 +8,7 @@ from .RealXarm7EnvBase import RealXarm7EnvBase
 
 
 class RealXarm7FixedGripperDemoEnv(RealXarm7EnvBase):
-    fixed_gripper_joint_pos = np.array([119.0], dtype=np.float64)
+    fixed_gripper_joint_pos = np.array([0.0], dtype=np.float64)
 
     def __init__(
         self,
