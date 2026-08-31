eb5b4053fbf38da6e5fd7ff4244cf046e7de3fa2
Kotaro-Kajitsuka-a <tactile.kajitsuka@aist.go.jp>
2026-08-31 11:42:28 +0900

    Added gripper obs and action for Pouch Learning

--- scripts/FutureImagination/Pouch/train_constant_pb_seed52.sh
+++ scripts/FutureImagination/Pouch/train_constant_pb_seed52.sh
@@ -19,8 +19,8 @@ python robo_manip_baselines/bin/Train.py DiffusionPolicy \
   --dataset_dir "$dataset_dir" \
   --checkpoint_dir "$checkpoint_dir" \
   --camera_names right \
-  --state_keys measured_eef_pose material_property \
-  --action_keys command_eef_pose \
+  --state_keys measured_eef_pose measured_gripper_pos material_property \
+  --action_keys command_eef_pose command_gripper_pos \
   --train_ratio 1.0 \
   --val_ratio 0.01 \
   --seed 52
