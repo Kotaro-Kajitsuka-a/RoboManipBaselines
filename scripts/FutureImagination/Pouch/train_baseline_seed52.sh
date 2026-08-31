82ed6d3a51af573e13873ec0b4bef69058b72496
kotaro-kajitsuka-a <tactile.kajitsuka@aist.go.jp>
2026-08-31 11:52:19 +0900

    Update state and action keys in training scripts to use joint position variables

--- scripts/FutureImagination/Pouch/train_baseline_seed52.sh
+++ scripts/FutureImagination/Pouch/train_baseline_seed52.sh
@@ -19,8 +19,8 @@ python robo_manip_baselines/bin/Train.py DiffusionPolicy \
   --dataset_dir "$dataset_dir" \
   --checkpoint_dir "$checkpoint_dir" \
   --camera_names right \
-  --state_keys measured_eef_pose measured_gripper_pos \
-  --action_keys command_eef_pose command_gripper_pos \
+  --state_keys measured_eef_pose measured_gripper_joint_pos \
+  --action_keys command_eef_pose command_gripper_joint_pos \
   --train_ratio 1.0 \
   --val_ratio 0.01 \
   --seed 52
