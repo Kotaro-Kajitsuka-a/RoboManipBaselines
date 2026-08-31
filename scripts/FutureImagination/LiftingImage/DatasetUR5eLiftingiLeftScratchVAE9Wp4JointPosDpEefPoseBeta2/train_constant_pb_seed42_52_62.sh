e97f60e72dc0ab7e873cbbc6e7f2a77b4b2d5e15
kotaro-kajitsuka-a <tactile.kajitsuka@aist.go.jp>
2026-08-31 09:12:47 +0900

    Add scripts for training and rollout of BetaVAE and Diffusion Policy models

---
+++ scripts/FutureImagination/LiftingImage/DatasetUR5eLiftingiLeftScratchVAE9Wp4JointPosDpEefPoseBeta2/train_constant_pb_seed42_52_62.sh
@@ -0,0 +1,36 @@
+#!/usr/bin/env bash
+set -e
+
+# Run after prepare_datasets.sh.
+dataset_dir=robo_manip_baselines/dataset/DatasetMujocoUR5eLiftingi_LeftScratchVAE9Beta2_Wp4JointPos_DpEefPose_ConstantPB
+checkpoint_prefix=robo_manip_baselines/checkpoint/DiffusionPolicy/DatasetMujocoUR5eLiftingi_LeftScratchVAE9Beta2_Wp4JointPos_DpEefPose_ConstantPB
+
+python robo_manip_baselines/bin/Train.py DiffusionPolicy \
+  --dataset_dir "$dataset_dir" \
+  --checkpoint_dir "${checkpoint_prefix}_seed42" \
+  --camera_names \
+  --state_keys measured_eef_pose measured_gripper_joint_pos measured_tblock_pose material_property \
+  --action_keys command_eef_pose command_gripper_joint_pos \
+  --train_ratio 1.0 \
+  --val_ratio 0.01 \
+  --seed 42
+
+python robo_manip_baselines/bin/Train.py DiffusionPolicy \
+  --dataset_dir "$dataset_dir" \
+  --checkpoint_dir "${checkpoint_prefix}_seed52" \
+  --camera_names \
+  --state_keys measured_eef_pose measured_gripper_joint_pos measured_tblock_pose material_property \
+  --action_keys command_eef_pose command_gripper_joint_pos \
+  --train_ratio 1.0 \
+  --val_ratio 0.01 \
+  --seed 52
+
+python robo_manip_baselines/bin/Train.py DiffusionPolicy \
+  --dataset_dir "$dataset_dir" \
+  --checkpoint_dir "${checkpoint_prefix}_seed62" \
+  --camera_names \
+  --state_keys measured_eef_pose measured_gripper_joint_pos measured_tblock_pose material_property \
+  --action_keys command_eef_pose command_gripper_joint_pos \
+  --train_ratio 1.0 \
+  --val_ratio 0.01 \
+  --seed 62
