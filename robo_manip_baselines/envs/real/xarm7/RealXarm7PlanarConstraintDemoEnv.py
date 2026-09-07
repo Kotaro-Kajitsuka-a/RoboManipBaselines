e13ffc8866074d24df0a3781ff240a2f18ce78e3
kotaro-kajitsuka-a <tactile.kajitsuka@aist.go.jp>
2026-09-07 17:19:24 +0900

    Restore xArm ready state after planar reset sensor zeroing

--- robo_manip_baselines/envs/real/xarm7/RealXarm7PlanarConstraintDemoEnv.py
+++ robo_manip_baselines/envs/real/xarm7/RealXarm7PlanarConstraintDemoEnv.py
@@ -94,6 +94,7 @@ class RealXarm7PlanarConstraintDemoEnv(RealXarm7FixedGripperDemoEnv):
 
         self.xarm_api.ft_sensor_set_zero()
         time.sleep(0.2)
+        self.xarm_api.set_state(0)
         print(
             f"[{self.__class__.__name__}] Finish moving the robot to the reset position."
         )
